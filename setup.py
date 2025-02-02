33bf0a76a7799b261dc1ad735969c3e4ab919533
Charles <dimpu.raj94@gmail.com>
2025-02-02 23:33:10 +0000

    added end points for the project

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+# Read requirements from requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='Sales_data_pipeline',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=required,  # Use the list of dependencies from requirements.txt
+    entry_points={
+        'console_scripts': [
+            'batch_upload=src.batch_upload:main',
+            'incremental_upload=src.incremental_upload:main',
+            'data_exploration=src.data_exploration:main',
+            'rough=src.rough:main'
+        ],
+    },
+)
